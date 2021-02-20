9226d5c22967489494b6b5475d4a6e8d77ea4bb5
Jonty Ward <jward0395@gmail.com>
2021-02-20 12:18:35 +0300

    created battleships- only displayes players

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,9 @@
 
 function init(){
 
+  //consts for the game
+
+  //consts for creating the grids
   const playerGrid = document.querySelector('.player-grid')
   const computerGrid = document.querySelector('.computer-grid')
   // const playerGrid = document.querySelector('.computer-grid')
@@ -9,9 +12,24 @@ function init(){
   const computerCells = []
   const gridWidth = 10 // if want 15 the width is 6%
   const gridCellCount =  gridWidth * gridWidth
+
+  //consts to create the ships 
+  const battleship = document.querySelector('.battleship')
+  const battleshipWidth = 4
+  // const battleshipCellCount =
+  const battleshipCells = []
+
+
   
 
-  function createComputerGrid(){
+
+
+
+
+
+  
+  //creating the computers playing grid 
+  function createComputerGrid(){  //could pass in a variable in the brackets 
     for (let i = 0; i < gridCellCount; i++){
       const cell = document.createElement('div') //creating the divs for the grid 
       cell.textContent = i //creating text withing the grid with the value of its index
@@ -22,6 +40,7 @@ function init(){
   }
 
 
+  // creating the players playing grid 
   function createPlayerGrid(){ //could pass in a variable in the brackets 
     for (let i = 0; i < gridCellCount; i++){
       const cell = document.createElement('div') //creating the divs for the grid 
@@ -35,9 +54,24 @@ function init(){
 
   createPlayerGrid() //calling the function to make the player grid 
   createComputerGrid() //calling the function to make the computergrid 
- 
-  console.log('player cells',playerGrid)
   
+  //creating the ships
+  function createBattleship(){
+    for (let i = 0; i < battleshipWidth; i++){
+      const cell = document.createElement('div')
+      cell.textContent = i
+
+      battleship.appendChild(cell)
+      battleshipCells.push(cell)
+    }
+  }
+  createBattleship()
+
+ 
+
+
+
+
 
 
 
