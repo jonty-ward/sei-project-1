9d1ec7d37891937143aac59b6798f94b0fde1ddd
Jonty Ward <jward0395@gmail.com>
2021-02-20 14:50:46 +0300

    able to select squares on computer grid, cannot add ships yet

--- scripts/app.js
+++ scripts/app.js
@@ -13,33 +13,30 @@ function init(){
   const gridWidth = 10 // if want 15 the width is 6%
   const gridCellCount =  gridWidth * gridWidth
 
-  //consts to create the ships 
-  const battleship = document.querySelector('.battleship')
-  const battleshipWidth = 4
-  // const battleshipCellCount =
-  const battleshipCells = []
+  //*consts to create the ships 
 
+  const battleship = document.querySelector('.battleship') //creating the battleship class
+  // const battleshipWidth = 4
+  // // const battleshipCellCount =
+  // const battleshipCells = []
+  // // console.log('battship', battleship)
+  // console.log(battleshipCells)
+
+  
 
   
 
-
-
-
-
-
-  
   //creating the computers playing grid 
   function createComputerGrid(){  //could pass in a variable in the brackets 
     for (let i = 0; i < gridCellCount; i++){
       const cell = document.createElement('div') //creating the divs for the grid 
       cell.textContent = i //creating text withing the grid with the value of its index
       //creating computer grid
+      cell.className += 'computerCell' //adding a class to all computer cells to add event listnerne r
       computerGrid.appendChild(cell)
       computerCells.push(cell)
     }
   }
-
-
   // creating the players playing grid 
   function createPlayerGrid(){ //could pass in a variable in the brackets 
     for (let i = 0; i < gridCellCount; i++){
@@ -50,22 +47,62 @@ function init(){
       playerCells.push(cell)
     }
     //this is where charlotte added the cat. 
+    
   }
-
   createPlayerGrid() //calling the function to make the player grid 
   createComputerGrid() //calling the function to make the computergrid 
   
-  //creating the ships
-  function createBattleship(){
-    for (let i = 0; i < battleshipWidth; i++){
-      const cell = document.createElement('div')
-      cell.textContent = i
 
-      battleship.appendChild(cell)
-      battleshipCells.push(cell)
-    }
+  //*creating the ships
+
+  // function createBattleship(){
+  //   for (let i = 0; i < battleshipWidth; i++){
+  //     const cell = document.createElement('div')
+  //     cell.textContent = i
+  
+  //     battleship.appendChild(cell)
+  //     battleshipCells.push(cell)
+  //   }
+  // }
+  // createBattleship()
+
+
+
+
+
+  //*adding ths ship to the player grid 
+
+  // make sure this is dry code
+
+  // function addShip(){ //*potential to add an input so this, and loop through the different ships eg addShip(ship), and pass in the ship vairable in a loop? 
+
+  //   playerCells[5].classList.add(battleship)
+    
+  // }
+
+
+  
+
+
+//* consts for event listener 
+  const targetComputerCell = document.querySelectorAll('.computerCell')
+
+
+  //*choosing a computer square 
+  function shootAtComputer(event){
+    console.log('function working' )
+    event.target.classList.add('shot-miss')
+    //need to be able to dissable the click on this square when clicked on
   }
-  createBattleship()
+
+
+
+
+
+  targetComputerCell.forEach(button =>{
+    button.addEventListener('click',shootAtComputer)
+  })
+  
 
  
 
