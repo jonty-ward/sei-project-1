8fec964dd29d5f82e518e941157300f3c880fde7
Jonty Ward <jward0395@gmail.com>
2021-02-20 09:24:05 +0300

    player and computer grid 10x10 made

--- scripts/app.js
+++ scripts/app.js
@@ -2,37 +2,42 @@
 function init(){
 
   const playerGrid = document.querySelector('.player-grid')
+  const computerGrid = document.querySelector('.computer-grid')
   // const playerGrid = document.querySelector('.computer-grid')
   //creating the size of the grid that is going to be used
   const playerCells = []
-  const gridWidth = 14
+  const computerCells = []
+  const gridWidth = 10 // if want 15 the width is 6%
   const gridCellCount =  gridWidth * gridWidth
-
-
-
-
   
 
-  
+  function createComputerGrid(){
+    for (let i = 0; i < gridCellCount; i++){
+      const cell = document.createElement('div') //creating the divs for the grid 
+      cell.textContent = i //creating text withing the grid with the value of its index
+      //creating computer grid
+      computerGrid.appendChild(cell)
+      computerCells.push(cell)
+    }
+  }
 
 
   function createPlayingGrid(){ //could pass in a variable in the brackets 
     for (let i = 0; i < gridCellCount; i++){
-
       const cell = document.createElement('div') //creating the divs for the grid 
       cell.textContent = i //creating text withing the grid with the value of its index
+      //creating the player grid 
       playerGrid.appendChild(cell)
       playerCells.push(cell)
-      
-
-
     }
-
     //this is where charlotte added the cat. 
-
   }
-  console.log('player grid',createPlayingGrid())
+
+  createPlayingGrid()
+  createComputerGrid()
+ 
   console.log('player cells',playerGrid)
+  
 
 
 
