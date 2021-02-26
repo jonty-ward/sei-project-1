0a80002b4322cac125439f65c8ae1e48c0480f5e
Jonty Ward <jward0395@gmail.com>
2021-02-26 09:58:28 +0300

    think ive fixed the vertical player placement bug

--- scripts/app.js
+++ scripts/app.js
@@ -296,7 +296,7 @@ function init(){
 
       // ** consts needed to make sure that player can only add ships if sdhering to certain conditions 
       const lastItemVert = (parseFloat(event.target.innerText) + (gridWidth * (shipClassToAdd.length - 1))  )
-      
+      console.log('last vertical item',lastItemVert)
       const secondLastItemVert = (parseFloat(event.target.innerText) + (gridWidth * (shipClassToAdd.length - 2))  )
       const thirdLastItemVert = (parseFloat(event.target.innerText) + (gridWidth * (shipClassToAdd.length - 3))  )
       const fourthLastItemVert = (parseFloat(event.target.innerText) + (gridWidth * (shipClassToAdd.length - 4))  )
@@ -314,7 +314,6 @@ function init(){
         for (let i = 0; i < shipClassToAdd.length; i++){
           targetPlayerCell[parseFloat(event.target.innerText) + (i )].classList.add('ship-outline')
         }
-        
         if (addingPlayerPieces[lastItem].classList.contains('ship') && lastItem >= parseFloat(event.target.innerText) || addingPlayerPieces[secondLastItem].classList.contains('ship') && secondLastItem >= parseFloat(event.target.innerText)  || addingPlayerPieces[thirdLastItem].classList.contains('ship') && thirdLastItem >= parseFloat(event.target.innerText) || addingPlayerPieces[fourthLastItem].classList.contains('ship') && fourthLastItem >= parseFloat(event.target.innerText) || addingPlayerPieces[fifthLastItem].classList.contains('ship') && fifthLastItem >= parseFloat(event.target.innerText) ){
           event.target.classList.add('mouse-hover-invalid')
         } else if (secondLastItem % gridWidth === 9 || thirdLastItem >= parseFloat(event.target.innerText) && thirdLastItem % gridWidth === 9 || fourthLastItem >= parseFloat(event.target.innerText) && fourthLastItem % gridWidth === 9 || fifthLastItem >= parseFloat(event.target.innerText) && fifthLastItem % gridWidth === 9){
@@ -332,15 +331,20 @@ function init(){
           }
         }
       } else if (shipClassToAdd.direction === 'vert'){           //* conditions for playe placing ship verically 
+
   
+        if (lastItemVert >= 99){
+          console.log('VERTICAL IS WORKING')
+
+          console.log('gone over the bottom ')
+          event.target.classList.add('mouse-hover-invalid')
+        } 
+
         for (let i = 0; i < shipClassToAdd.length; i++){
           targetPlayerCell[parseFloat(event.target.innerText) + (i * gridWidth)].classList.add('ship-outline')
         }
 
-        if (lastItemVert >= 99){
-          console.log('gone over the bottom ')
-          event.target.classList.add('mouse-hover-invalid')
-        } 
+        
         // console.log('last item vert',typeof(lastItemVert))
         if (addingPlayerPieces[lastItemVert].classList.contains('ship') || addingPlayerPieces[secondLastItemVert].classList.contains('ship') || addingPlayerPieces[thirdLastItemVert].classList.contains('ship') && thirdLastItem >= parseFloat(event.target.innerText) || addingPlayerPieces[fourthLastItemVert].classList.contains('ship') && fourthLastItem >= parseFloat(event.target.innerText) || addingPlayerPieces[fifthLastItemVert].classList.contains('ship') && fifthLastItem >= parseFloat(event.target.innerText) ){
           // console.log('vertical ship category selector ')
